3d517c412339449af4fbe51dada7c255329b46d9
AL <aldmckay@gmail.com>
2021-08-24 01:28:10 -0400

    fixed tests

---
+++ tests/test_services.py
@@ -0,0 +1,44 @@
+"""Test cases for the ebird module."""
+import pytest
+import requests
+from click.testing import CliRunner
+
+from ibyrd.service import ebird
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture for invoking command-line interfaces."""
+    return CliRunner()
+
+
+def test_get_hotspots_valid_coord(runner: CliRunner) -> None:
+    """Test hotspot functionality."""
+    function_response = ebird.get_hotspots(47.60, -122.33, fmt="json")
+
+    parameters = {"lat": 47.60, "lng": -122.33, "fmt": "json", "dist": 10, "back": 5}
+    response = requests.get(
+        "https://api.ebird.org/v2/ref/hotspot/geo?", params=parameters
+    )
+
+    locations = []
+    for result in response.json():
+        locations.append(str(result["locName"]))
+
+    assert function_response == locations
+
+
+# def test_get_hotspots_invalid_coord(runner: CliRunner) -> None:
+#     """Test hotspot functionality."""
+#     response = ebird.get_hotspots(91, 181, fmt="json")
+#     assert response.status_code == 400
+
+# @pytest.fixture
+# class TestEbirdService(unittest.TestCase):
+#     """Test Cases to validate API calls to Ebird."""
+
+#     @patch("ibyrd.service.ebird.get_hotspots.request")
+#     def test_get_hotspots_invalid_coord(self) -> None:
+#         """Test hotspot functionality."""
+#         response = ebird.get_hotspots(91, 181, fmt="json")
+#         self.assertEqual(response.status_code, 400)
