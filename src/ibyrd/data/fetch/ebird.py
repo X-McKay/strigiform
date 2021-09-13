bf0c89489106067233a046a868a95c9563b7ade6
AL <aldmckay@gmail.com>
2021-09-13 12:26:36 -0700

    updating unit tests

--- src/ibyrd/data/fetch/ebird.py
+++ src/ibyrd/data/fetch/ebird.py
@@ -1,32 +1,12 @@
 """Module to interact with EBird API."""
 import json
 import os
-from typing import Any
-
-import requests
 
 from ibyrd.util import config
 from ibyrd.util.api import api_extract
 
 
 ebird_key = os.getenv("EBIRD_KEY")
-
-
-def get_hotspots(
-    lat: float, lon: float, fmt: str = "json", dist: int = 10, back: int = 5
-) -> Any:
-    """Retreive HotStop info for Lat/Lon coordinates."""
-    parameters = {"lat": lat, "lng": lon, "fmt": fmt, "dist": dist, "back": back}
-
-    response = requests.get(
-        "https://api.ebird.org/v2/ref/hotspot/geo?", params=parameters  # type: ignore
-    )
-
-    locations = []
-    for result in response.json():
-        locations.append(str(result["locName"]))
-
-    return locations
 
 
 def get_taxonomy(
@@ -54,22 +34,7 @@ def get_taxonomy(
     return api_extract(config.EBIRD_TAXONOMY_URL, params, save, save_fmt, path)
 
 
-# def get_checklist(subId: str) -> Any:
-#     """Retreive eBird checklist information based on a single ID."""
-#     header = {"X-eBirdApiToken": EBIRD_KEY}
-#     parameters = {"subId": subId}
-#     subId = "S90521630"
-#     response = requests.get(
-#         f"https://api.ebird.org/v2/product/checklist/view/{subId}", headers=header
-#     )
-#     taxonomy = StringIO(response.text)
-#     df = pd.read_csv(taxonomy)
-
-# TODO: def get_ebird_region():
-# TODO: def get_species_list():
-
-
-def get_hots(
+def get_hotspots(
     lat: float = config.DEFAULT_LAT,
     lng: float = config.DEFAULT_LNG,
     fmt: str = config.DEFAULT_FORMAT,
@@ -102,3 +67,18 @@ def get_hots(
         hotspots = response
 
     return hotspots
+
+
+# def get_checklist(subId: str) -> Any:
+#     """Retreive eBird checklist information based on a single ID."""
+#     header = {"X-eBirdApiToken": EBIRD_KEY}
+#     parameters = {"subId": subId}
+#     subId = "S90521630"
+#     response = requests.get(
+#         f"https://api.ebird.org/v2/product/checklist/view/{subId}", headers=header
+#     )
+#     taxonomy = StringIO(response.text)
+#     df = pd.read_csv(taxonomy)
+
+# TODO: def get_ebird_region():
+# TODO: def get_species_list():
