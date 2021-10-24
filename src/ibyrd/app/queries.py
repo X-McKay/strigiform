440fda479aebdd684f660aec989cc0d8e4f7d559
AL <aldmckay@gmail.com>
2021-10-24 16:46:02 -0400

    clean-up

--- src/ibyrd/app/queries.py
+++
@@ -1,8 +0,0 @@
-"""Queries for webapp."""
-
-observation_query = """
-                        select t.*, o.date
-                        from taxonomy t
-                        left join observations o
-                        on o.taxon_order = t.taxon_order
-                    """
